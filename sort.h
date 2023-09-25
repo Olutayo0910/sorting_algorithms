45158254bab4b24dce5b625b68a554f7837cb3ff
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-09-25 07:25:33 -0700

    task 0

--- sort.h
+++ sort.h
@@ -1,25 +1,20 @@
-#ifndef SORT_H_
-#define SORT_H_
-
-#include <stddef.h>
+#ifndef _SORT_H
+#define _SORT_H
 #include <stdlib.h>
 #include <stdio.h>
-
 /**
  * struct listint_s - Doubly linked list node
+ *
  * @n: Integer stored in the node
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-	const int n;
-	struct listint_s *prev;
-	struct listint_s *next;
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
 } listint_t;
-
-/* Function prototypes for all sorting algorithms */
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void insertion_sort_list(listint_t **list);
