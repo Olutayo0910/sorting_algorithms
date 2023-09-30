c45062185c652d4ab6b8b18a8e759b4a5f18a87f
Olutayo0910 <olutayoogunlade0910@gmail.com>
2023-09-29 22:06:07 -0700

    task 5

---
+++ 100-shell_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * shell_sort -Sort an array using shell sort
+ * @array: array
+ * @size: size of the array
+ * Return: NULL
+ **/
+void shell_sort(int *array, size_t size)
+{
+	unsigned int space = 1;
+    unsigned i, j;
+	int aux;
+
+	if (array == NULL)
+		return;
+	if (size < 2)
+		return;
+	while (space < size / 3)
+		space = space * 3 + 1;
+
+	while (space > 0)
+	{
+		for (i = space; i < size; i++)
+		{
+			aux = array[i];
+			j = i;
+			while (j >= space && array[j - space] > aux)
+			{
+				array[j] = array[j - space];
+				j -= space;
+			}
+			array[j] = aux;
+		}
+		print_array(array, size);
+		space /= 3;
+	}
+}
